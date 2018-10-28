8c72be72c5c239089df4571457cf6d2f2e452098
Naman Vashistha <namanvashistha15@gmail.com>
2018-10-29 01:04:00 +0530

    Update home.php

--- home.php
+++ home.php
@@ -16,65 +16,14 @@ $q1=mysqli_query($con,$q);
 	<link rel="stylesheet" type="text/css" href="css/home.css">
 	<title>home page</title>
 	<link rel="shortcut icon" href="logo.png" type="image/png">
-
-<style>
-body{
-	margin: 0px;
-
-}
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: black;
-}
-
-li {
-    float: left;
-}
-
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    text-decoration: none;
-}
-
-li a:hover:not(.active) {
-    background-color: #111;
-}
-
-.active {
-    background-color: golden
-    text-align: center;
-
-.arrow-down{
-
-    width: 0; 
-  height: 0; 
-  border-left: 5px solid transparent;
-  border-right: 5px solid transparent;
-  
-  border-top: 5px solid black;
-
-}
-
-</style>
-
 </head>
 
-<body>
+<body style="font-family: Helvetica;">
 <ul>
 <li><img src="header_logo.jpeg" align="left" width="100" height="52"></li>
-  
 </ul>
-
-
 <center><h4>Restaurants</h4></center>
 	
-
-<body style="font-family: Helvetica;">
 	<a href="logout.php"><button>logout</button></a>
 
 	<div class="recommendation">
