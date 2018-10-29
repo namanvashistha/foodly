31d5e2a9a22e039e8e8c1467acbc343be2b04997
bhavyamendiratta <42599132+bhavyamendiratta@users.noreply.github.com>
2018-10-30 04:02:42 +0530

    Frontend updated

---
+++ js/restaurant_sign.js
@@ -0,0 +1,17 @@
+
+var log_error=document.getElementById("log_error_msg").innerHTML ;
+var sign_error = document.getElementById("sign_error_msg").innerHTML;
+
+if (log_error=="incorrect email or password") {
+	var log_pop = document.getElementById('id01');
+	log_pop.style.display="block";
+}   
+
+if (sign_error=="email already exists") {
+	var sign_pop = document.getElementById('id02');
+	sign_pop.style.display="block";
+}
+
+
+
+
