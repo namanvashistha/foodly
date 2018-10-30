b9a5fd8b91dbfce8c8bcf1b8da483afa0b27a621
namanvashistha <namanvashistha15@gmail.com>
2018-10-30 11:25:59 +0530

    cards on the home are updated

--- home.php
+++ home.php
@@ -23,23 +23,18 @@ $q1=mysqli_query($con,$q);
 <li><img src="images\header_logo.jpeg" align="left" width="100" height="52"></li>
 </ul>
 <center><h2>Restaurants</h2></center>
-	
-	
-    <div class="card">
-  <img src="FETCH THE IMAGE WITH PHP" alt="res_photo" style="width:100%">
-  <div class="container">
-    <h4><b>RESTAURANT NAME</b></h4> 
-    <p>Status</p> 
-  </div>
-
-		<?php
-			while($row=mysqli_fetch_array($q1)){ ?>
-				<a href="restaurant_menu.php?restaurant=<?php echo $row['email']; ?>">
-				<div class="list">	
-				<?php echo $row['name']." ".$row['status'];  ?>
-				</div></a>
-		<?php } ?>
-	</div>
+<div>
+	<?php
+	while($row=mysqli_fetch_array($q1)){ ?>
+		<a href="restaurant_menu.php?restaurant=<?php echo $row['email']; ?>">
+    		<div class="card">
+    			<div class="container">
+    				<h4><b><?php echo $row['name'];  ?></b></h4> 
+    				<p><?php echo $row['status'];  ?></p>
+    			</div>
+    		</div>
+  		</a><?php } ?>
+</div>
 
 
  <div class="navbar">
