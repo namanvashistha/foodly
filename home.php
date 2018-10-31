be7abc0ce822288c01a95198641a9dcf98538989
bhavyamendiratta <42599132+bhavyamendiratta@users.noreply.github.com>
2018-10-31 20:27:05 +0530

    minor changes

--- home.php
+++ home.php
@@ -11,6 +11,7 @@ $q1=mysqli_query($con,$q);
 <!DOCTYPE html>
 <html>
 <meta charset="UTF-8">
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
 	<link rel="stylesheet" type="text/css" href="css/home.css">
