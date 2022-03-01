dba90aaadb8be37dfe6652f955d567873769cb19
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-01 17:40:21 +0800

    Delete send_msg.php

--- send_msg.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'connection.php';
- $msg=$_POST["msg"];
- $client=$_SESSION["log_client"] ;
- $q_snd="INSERT INTO chat_support (`client`,`txt`) values ('$client','$msg') ";
- mysqli_query($con,$q_snd);
-?>
